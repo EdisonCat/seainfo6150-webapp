f389a97adec032aed7a9f80a0d511281a0550b2e
EdisonCat <edisonguocat@gmail.com>
2021-02-28 03:20:48 -0800

    js assignment

--- src/App.js
+++ src/App.js
@@ -4,39 +4,44 @@ import DynamicArticle from "./DynamicArticle/DynamicArticle.jsx";
 import { isEmpty } from "lodash";
 
 function App() {
-  const [fetchedData, setFetchedData] = useState();
+	const [fetchedData, setFetchedData] = useState();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let responseJson;
-      // put data fetching code here!
-      setFetchedData(responseJson);
-    };
+	useEffect(() => {
+		const fetchData = async () => {
+			const responseJson = await (
+				await fetch("https://demo1390455.mockable.io/articles")
+			).json();
+			setFetchedData(responseJson);
+		};
 
-    if (isEmpty(fetchedData)) {
-      fetchData();
-    }
-  }, [fetchedData]);
+		if (isEmpty(fetchedData)) {
+			fetchData();
+		}
+	}, [fetchedData]);
 
-  return isEmpty(fetchedData) ? null : (
-    <div className="App">
-      <Switch>
-        <Route
-          exact
-          path={`/articlelist/:slug`}
-          render={({ match }) => {
-            // getting the parameters from the url and passing
-            // down to the component as props
-            console.log("this slug", match.params.slug);
-            return <div>Component</div>;
-          }}
-        />
-        <Route>
-          <DynamicArticle article={Object.values(fetchedData)[1]} />
-        </Route>
-      </Switch>
-    </div>
-  );
+	return isEmpty(fetchedData) ? null : (
+		<div className="App">
+			<Switch>
+				<Route
+					exact
+					path={`/articlelist/:slug`}
+					render={({ match }) => {
+						return (
+							<DynamicArticle
+								article={fetchedData[match.params.slug]}
+							/>
+						);
+					}}
+				/>
+				<Route>
+					<DynamicArticle article={Object.values(fetchedData)[1]}/>
+					{/* {Object.values(fetchedData).map((article, idx) => (
+						<DynamicArticle key={idx} article={article} />
+					))} */}
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import HTMLText from "../HTMLText/HTMLText";
 
 const DynamicArticle = (props) => {
-  return (
-    <article>
-      {/* add extra html with javascript attributes here */}
-      <HTMLText text={props.article.text} />
-    </article>
-  );
+	return (
+		<article>
+			{/* add extra html with javascript attributes here */}
+			<header>
+				<h1>{props.article.title}</h1>
+				<address>
+					{props.article.author}
+					<br />
+					(<a href={props.article.authorEmail}>{props.article.authorEmail}</a>)
+				</address>
+				<time dateTime={props.article.timeStamp}>
+					{props.article.displayDate}
+				</time>
+			</header>
+			<HTMLText text={props.article.text} />
+		</article>
+	);
 };
 
 export default DynamicArticle;
